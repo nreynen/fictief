d098337dd03d001e01260f9e27c6b618f485ce01
Nico Reynen <nreynen@helionresearch.com>
2012-10-21 12:46:19 +0200

    mail fix

--- lib/reminder_mailer.rb
+++ lib/reminder_mailer.rb
@@ -9,11 +9,17 @@ class ReminderMailer
     sat = now + ((5 - now.wday) % 7)
     users = User.all.map(&:id)
     users_with = User.with_order(sat).find(:all, :select => "users.id").map(&:id)
+    
+    
     users_without = users - users_with
-    puts "#{users_without.length} shoppers found"
+    
+    
     users_without = [1]
+    puts "#{users_without.length} shoppers found"
+    
     begin
       User.find(:all, :conditions => ["id IN (?)", users_without]).each do |usr|
+        puts "Mailing user #{usr.email}"
         MasterMailer.deliver_bread_alert({
           :user => usr, 
           :subject => "Bread Reminder: You haven't created an order!", 
