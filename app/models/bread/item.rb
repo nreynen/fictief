97213cdc186d562007f9a9c8f100f568be8f6c4f
Nico Reynen <nreynen@helionresearch.com>
2013-04-07 17:47:26 +0200

    namespaces pt2

--- app/models/bread/item.rb
+++ app/models/bread/item.rb
@@ -1,6 +1,6 @@
 class Bread::Item < ActiveRecord::Base
   
-  belongs_to :category
+  belongs_to :category, :class_name => "Bread::Category"
   
   validates_presence_of :category_id
   validates_presence_of :name
