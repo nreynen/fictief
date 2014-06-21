0084fab5613e5b492d1c5097cdad8bfed6bd1581
Nico Reynen <nreynen@helionresearch.com>
2014-06-21 10:33:03 +0200

    action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ MasterControl::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "yato-extreme.com",
+    :authentication => :plain,
+    :user_name => "noreply@yato-extreme.com",
+    :password => "noreply_extreme", 
+    :enable_starttls_auto => true
+  }
 end
